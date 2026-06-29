efed90bd4f89f1a56bf58f57ba9d76733c79fbfe
TQB666 <baotz147@gmail.com>
2026-06-29 20:52:59 +0700

    create features and specs sections

--- smartwatch-landing/src/pages/Home.tsx
+++ smartwatch-landing/src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import Navbar from "../components/layout/Navbar";
 import Hero from "../components/sections/Hero";
-
+import Features from "../components/sections/Features";
+import Specs from "../components/sections/Specs";
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Features/>
+      <Specs/>
     </>
   );
 };
