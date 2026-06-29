635b61b04410f8b606fd3b2d78790965cb06a758
TQB666 <baotz147@gmail.com>
2026-06-29 20:21:51 +0700

    setup tailwind v4 and project structure

---
+++ smartwatch-landing/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/layout/Navbar";
+import Hero from "../components/sections/Hero";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar/>
+            <Hero/>
+        </>
+    )
+}
+
+export default Home;
